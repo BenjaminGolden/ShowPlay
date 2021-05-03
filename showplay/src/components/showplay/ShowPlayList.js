aa8b1413bf05ec5b0f2724709064c4f856d8caa6
BenjaminGolden <b.j.golden@outlook.com>
2021-05-02 21:10:14 -0500

    search bar working to search by event name

--- showplay/src/components/showplay/ShowPlayList.js
+++ showplay/src/components/showplay/ShowPlayList.js
@@ -9,13 +9,16 @@ export const MainList = () => {
     const [activities, setActivities] = useState([])
     const [categories, setCategories] = useState([])
     const [filterId, setFilterId] = useState(0)
+    const [search, setSearch] = useState([])
     const currentUser = parseInt(sessionStorage.getItem("app_user_id"))
 
     const getActivitiesForCurrentUser = (id) => {
         return getActivitiesByUserId(id)
         .then(eventsFromAPI => {
             setActivities(eventsFromAPI)
+            setSearch(eventsFromAPI)
         })
+
     }
 
     const getCategories = () => {
@@ -31,12 +34,21 @@ export const MainList = () => {
 
     }
 
-    // const handleFieldChange = (evt) => {
-    //     let stateToChange = { ...activities }
-    //     stateToChange = evt.target.value
-    //     setActivities(stateToChange)
-    
-    // }
+    const handleSearch =(evt) => {
+        evt.preventDefault()
+        let searchInput = evt.target.value
+        
+        if (searchInput.length > 0) {
+            let searchMatch = activities.filter(activity => {
+                if (activity.name.toLowerCase().includes(searchInput.toLowerCase())) {
+                    return true
+                }
+            })
+            setSearch(searchMatch)
+        } else {
+            setSearch(activities)
+        }
+    }
 
     // sort data
 
@@ -56,14 +68,18 @@ export const MainList = () => {
     useEffect(() => {
         getActivitiesForCurrentUser(currentUser);
         getCategories();
+        
     }, [currentUser]);
 
 
 
     return(
         <>
-        {/* <div className="event__cards">  
-            <input type='text' className="search" required onChange={handleFieldChange} id="search_box" placeholder="Search"/>
+        <div className="event__cards">  
+            <input type='text' className="search" required onChange={handleSearch} id="search_box" placeholder="Search"/>
+        </div>
+        {/* <div>
+            {search.map(activity => <MainCard key={activity.id}activity={activity}/> )}
         </div> */}
         <div>
             <select value={categories.name} name="category" className='form-control' id="category" onChange={handleActivityFilter}>
@@ -75,8 +91,8 @@ export const MainList = () => {
         </div>
         <div className="section__content">
             { filterId === 0
-            ? activities.map(activity => <MainCard key={activity.id} activity={activity} />)
-            : activities.filter(activity => activity.categoryId === filterId).map(activity => <MainCard key={activity.id} activity={activity} />)
+            ? search.map(activity => <MainCard key={activity.id} activity={activity} />)
+            : search.filter(activity => activity.categoryId === filterId).map(activity => <MainCard key={activity.id} activity={activity} />)
             }
 
         </div>
