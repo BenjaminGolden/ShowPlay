156a4f13f3e02868617a6600db3358ba4bd75af5
BenjaminGolden <b.j.golden@outlook.com>
2021-05-06 11:37:19 -0500

    css fixes and db url updates

--- showplay/src/components/showplay/ShowPlayDetails.js
+++ showplay/src/components/showplay/ShowPlayDetails.js
@@ -25,21 +25,22 @@ export const EventDetail = () => {
         .then(() => history.push('/'))
     };
 
+    const url = `http://${activity.url}`
+    //TODO: url gets added to the end of the existing url instead of going to the external link
     return(
         <section className="details">
             <div className="details__card">
-            <div className="details__rating"> <h3 className="details__name">{activity.name}</h3>
-            <b>rating: </b>{"★".repeat(activity.rating)} </div>
-            <div className="details__location"><b>Location: </b>{activity.city}, {activity?.state?.name}</div>
-            <div className="details__category"><b>Category: </b>{activity?.category?.name}</div>
-            <div className="details__description"><b>description: </b>{activity.description}</div>
-            <div className="details__date"><b>date: </b>{activity.date}</div>
-            <div className="details__url"><b>url: </b> <a className=
-            "details__url--link" href={activity.url}>{activity.url}</a></div>
-            <div className="details__button">
-            <button type="button" disabled={isLoading} onClick={handleDelete}>Delete</button>
-            <button type="button"onClick={() => history.push(`/events/${activity.id}/edit`)}>Edit</button>
-            </div>
+                <div className="details__rating"> <h3 className="details__name">{activity.name}</h3>
+                <b>Rating: </b>{"★".repeat(activity.rating)} </div>
+                <div className="details__location"><b>Location: </b>{activity.city}, {activity?.state?.name}</div>
+                <div className="details__category"><b>Category: </b>{activity?.category?.name}</div>
+                <div className="details__description"><b>Description: </b>{activity.description}</div>
+                <div className="details__date"><b>Date: </b>{activity.date}</div>
+                <div className="details__url"><b>URL: </b> <a className="details__url--link" href={url}>{activity.url}</a></div>
+                <div className="details__button">
+                <button type="button" disabled={isLoading} onClick={handleDelete}>Delete</button>
+                <button type="button"onClick={() => history.push(`/events/${activity.id}/edit`)}>Edit</button>
+                </div>
             </div>
         </section>
     )
